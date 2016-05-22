f3c5885e5062df4095a1740b54c43d98d04da8fd
sophia <szensius@gmail.com>
2016-05-21 22:54:41 -0700

    adding app key

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,8 +13,9 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+    'App\Console\Commands\KeyGenerateCommand',
     ];
+
 
     /**
      * Define the application's command schedule.
